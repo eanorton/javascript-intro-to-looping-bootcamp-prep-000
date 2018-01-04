ac8980a7f05215972c643bf246ece1dcb7d1d768
Edward Norton <edwardnorton@icloud.com>
2018-01-04 22:11:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,6 +21,8 @@ function doWhileLoop(array) {
     return Math.random() >= 0.5 
   }
   do {
-    array = array.slice(1) }
-    while array.length > 0 && maybeTrue())
+    array = array.slice(1) 
+  }
+    while array.length > 0 && maybeTrue()
+    return array
 }
